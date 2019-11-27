b0850368abf5e52f8ab66fa43efe3e71805886ee
vladislav <bincha.1997@gmail.com>
2019-11-27 23:34:55 +0300

    Added error handling

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import flask
+from flask import jsonify, abort
 import os
 
 app = flask.Flask(__name__)
@@ -12,5 +13,40 @@ def convert():
         "Hello": "World"
     }
     return data
+
+
+@app.errorhandler(400)
+def bad_request_error(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 400
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 500
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 500
+
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 404
+
+
 
 app.run(debug=True)
