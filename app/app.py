073957d79fa2ef4333b1b1b6f196fbc846695bf6
vladislav <bincha.1997@gmail.com>
2019-11-29 23:41:30 +0300

    Update app

--- app/app.py
+++ app/app.py
@@ -4,11 +4,12 @@ import os
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-app.config["SECRET_KEY"] = "it-is-secret-key"
+
+
 
 
 @app.route("/api/v1/sending_messages", methods=["GET"])
-def convert():
+def sending_messages():
     data = {
         "Hello": "World"
     }
@@ -39,14 +40,12 @@ def method_not_allowed(error):
     return jsonify(message), 500
 
 
-
 @app.errorhandler(404)
 def not_found_error(error):
     message = {
         "error": f"{error}",
     }
     return jsonify(message), 404
-
 
 
 app.run(debug=True)
