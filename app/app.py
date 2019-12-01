30b0fef7516ec5b1025a362cde4ab6f50886c8d0
vladislav <bincha.1997@gmail.com>
2019-12-01 23:42:31 +0300

    Added routes.py

--- app/app.py
+++ app/app.py
@@ -1,53 +1,11 @@
 import flask
-from flask import jsonify, abort
 import os
 
 app = flask.Flask(__name__)
-db = SQLAlchemy(app)
-
 app.config["DEBUG"] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config.from_object(os.environ['APP_SETTINGS'])
 
 
-@app.route("/api/v1/sending_messages", methods=["GET"])
-def sending_messages():
-    data = {
-        "Hello": "World"
-    }
-    return data
+import routes
 
-
-@app.errorhandler(400)
-def bad_request_error(error):
-    message = {
-        "error": f"{error}",
-    }
-    return jsonify(message), 400
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    message = {
-        "error": f"{error}",
-    }
-    return jsonify(message), 500
-
-
-@app.errorhandler(405)
-def method_not_allowed(error):
-    message = {
-        "error": f"{error}",
-    }
-    return jsonify(message), 500
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    message = {
-        "error": f"{error}",
-    }
-    return jsonify(message), 404
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/routes.py
@@ -0,0 +1,42 @@
+from app import app
+from flask import jsonify, abort
+
+
+@app.route("/api/v1/sending_messages", methods=["GET"])
+def sending_messages():
+    data = {
+        "Hello": "World"
+    }
+    return data
+
+
+@app.errorhandler(400)
+def bad_request_error(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 400
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 500
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    message = {
+        "error": f"{error}",
+    }
+    return jsonify(message), 404
